c31d2fd7919dd8e69ed8c8d84793c28fb4c61b2f
devBandeiraa <jhordanbandeira1@gmail.com>
2025-10-30 15:18:46 -0300

    Implementado Spring Data JPA

--- src/main/java/br/com/jhordan/screenmatch/model/Episodio.java
+++ src/main/java/br/com/jhordan/screenmatch/model/Episodio.java
@@ -1,17 +1,28 @@
 package br.com.jhordan.screenmatch.model;
 
-import java.security.spec.ECParameterSpec;
-import java.time.DateTimeException;
+
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+
+@Entity
+@Table(name = "episodios")
 public class Episodio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataLancamento;
+
+    @ManyToOne
+    private Serie serie;
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio){
         this.temporada = numeroTemporada;
@@ -31,6 +42,32 @@ public class Episodio {
         catch (DateTimeParseException ex){
             this.dataLancamento = null;
         }
+    }
+
+    public Episodio(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 
     public Integer getTemporada() {
